028efb22437c1c3422dfd75b9e07ecd1eed4f2a0
mwongess <amosmwongelah@gmail.com>
2023-05-30 21:42:30 +0300

    Add the side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { SideNav } from './components/sidenav/SideNav'
 import { TodoProvider } from './contexts/context'
 
 function App() {
       <TodoProvider>
-        
+        <SideNav/>
       </TodoProvider>
 }
 
