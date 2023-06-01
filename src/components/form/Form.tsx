581ce305f2a009446ba1d9a9081ce8b83619d9d5
mwongess <amosmwongelah@gmail.com>
2023-06-01 13:35:09 +0300

    Define form action

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,13 +1,24 @@
-import { Form } from 'react-router-dom';
-import { FaPlus } from 'react-icons/fa';
+import { Form } from "react-router-dom";
+import { FaPlus } from "react-icons/fa";
 
-export const NewTask: React.FC = () =>{
-    return(
-        <div>
-            <Form method='post' action=''>
-                <input type="text" name="" placeholder="Title" />
-                <button> <FaPlus/> Add Task</button>
-            </Form>
-        </div>
-    )
-}
+export const NewTask: React.FC = () => {
+  return (
+    <div>
+      <Form method="post" action="">
+        <input type="text" name="title" placeholder="New Task" />
+        <button>
+          {" "}
+          <FaPlus /> Add Task
+        </button>
+      </Form>
+    </div>
+  );
+};
+
+export const formAction = async ({ request }: any) => {
+  const data = await request.formData();
+  const submission: { title: string } = {
+    title: data.get("title"),
+  };
+  console.log(submission);
+};
