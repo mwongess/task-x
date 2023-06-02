51908c765c6591b752223edc98251819728feb83
mwongess <amosmwongelah@gmail.com>
2023-06-02 10:34:51 +0300

    Setup auth form

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -1,17 +1,50 @@
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Form } from "react-router-dom";
+import { useState } from "react";
 
-const Auth = () => {
+export const Auth = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [formData, setFormData] = useState<{ email: string; password: string }>(
+    {
+      email: "",
+      password: "",
+    }
+  );
+
   const onSubmit = async () => {
     try {
       const auth = getAuth();
-      const userCredential = await createUserWithEmailAndPassword(auth)
-      const user = userCredential.user
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        formData.email,
+        formData.password
+      );
+      const user = userCredential.user;
     } catch (error) {
-        console.error(error)
+      console.error(error);
     }
   };
-  
-   
+  return (
+    <>
+      <div className="auth-form">
+        <Form action="/auth" method="post">
+          {!isLoginMode && <input type="text" placeholder="Name" />}
+          <input type="text" placeholder="Email" />
+          <div className="auth-password">
+            <input
+              type={showPassword ? "password" : "text"}
+              placeholder="Password"
+            />
+            {/* Icon to toggle show password */}
+          </div>
+          <button>{isLoginMode ? "Login" : "Signup"}</button>
+        </Form>
+      </div>
+    </>
+  );
 };
 
-export default Auth;
+const authFormAction = ()=> {
+
+}
