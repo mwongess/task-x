0fcb2e6b240e538aee8ba6aa5b59e84566b4cf2f
mwongess <amosmwongelah@gmail.com>
2023-06-05 12:02:41 +0300

    Setup auth route action

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
-import dotenv from 'dotenv'
-dotenv.config()
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
@@ -9,7 +8,7 @@ const firebaseConfig = {
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  appId: process.env.APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
